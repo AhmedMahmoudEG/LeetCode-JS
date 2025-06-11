1aef4be83b2eae70e2191d219babccbb59659253
Ahmed Mahmoud <60559798+YamatoEG@users.noreply.github.com>
2025-06-11 23:25:47 +0300

    [56] [Time Beats: 87.07%] [Memory Beats: 24.11%] - LeetPush

---
+++ Merge-Intervals.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    //first we need to sort the intervals 
+    //space O(1)
+    //time O(nlogn)
+    intervals = intervals.sort((a,b)=>a[0]-b[0]);
+    let start =0 ;
+    let end = 1;
+    let previous = intervals[0]
+    let res = [previous]
+    //draw it to figure out the overlap and the conditions 
+    //if prev[end]>=current[start] // means overlapping 
+    //else push to reslut and update the previous
+    for(let current of intervals){
+        if(previous[end]>=current[start]){
+            previous[end] = Math.max(previous[end],current[end])
+        }else{
+            res.push(current);
+            previous= current
+        }
+    }
+    return res;
+};
