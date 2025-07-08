bcdc477f84d239653684c2169da78cfa84c8992b
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-07-08 21:03:04 +0300

    [257] [Time Beats: 100.00%] [Memory Beats: 18.02%] - LeetPush

---
+++ Binary-Tree-Paths.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    const result = []
+    function dfs(node,path){
+        if(!node) return 
+        path += node.val.toString()
+        if(!node.left&&!node.right){
+                result.push(path)
+                return
+        }
+        dfs(node.left, path + "->");
+        dfs(node.right, path + "->");
+    }
+     dfs(root,result)
+    return result
+};
