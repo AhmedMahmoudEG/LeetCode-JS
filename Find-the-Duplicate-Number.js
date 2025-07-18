37950afa90ea88b2921cfd5c6da04160cd34ab10
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-07-18 23:58:50 +0300

    [287] [Time Beats: 32.11%] [Memory Beats: 11.39%] - LeetPush

--- Find-the-Duplicate-Number.js
+++ Find-the-Duplicate-Number.js
@@ -3,16 +3,12 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let slow = nums[0]
-    let fast = nums[0]
-    do{
-        slow = nums[slow]
-        fast = nums[nums[fast]]
-    }while(slow!=fast)
-    slow=nums[0]
-    while(slow!==fast){
-            slow = nums[slow]
-            fast = nums[fast]
+    let set = new Set();
+    for(let i =0;i<nums.length;i++){
+        if(set.has(nums[i])){
+            return nums[i]
+        }else{
+            set.add(nums[i])
+        }
     }
-    return slow
 };
