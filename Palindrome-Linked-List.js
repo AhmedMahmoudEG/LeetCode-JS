7157364169ab47500dc2d340bb75f46178425fc5
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-07-28 01:38:32 +0300

    [234] [Time Beats: 99.74%] [Memory Beats: 77.38%] - LeetPush

--- Palindrome-Linked-List.js
+++ Palindrome-Linked-List.js
@@ -10,6 +10,36 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
+
+    //using two pointers 
+    //first we need to get the middle of the list using fast and slow pointer
+    let fast = head
+    let slow = head
+    while(fast !== null && fast.next !== null){
+        fast = fast.next.next
+        slow = slow.next
+    }
+    // Reverse the second half of the list
+    let secondHalf = (fast === null) ? slow : slow.next;
+    let prev = null
+    while(secondHalf){
+        let nextP = secondHalf.next
+        secondHalf.next = prev
+        prev = secondHalf
+        secondHalf = nextP
+    }
+    //compare the two halfs 
+    let p1 = head;     // start of first half
+    let p2 = prev;     // start of reversed second half
+    while(p2!=null){ //compare until the second half ends
+        if(p1.val !=p2.val) return false
+        p1= p1.next;
+        p2 = p2.next
+    }
+    return true
+    
+
+    /*
     let current = head
     let arr = []
     while(current){
@@ -29,4 +59,5 @@ var isPalindrome = function(head) {
         }
     }
     return true
+    */
 };
