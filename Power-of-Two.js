cf01c56a5123cfe4973b3d9245c571c12b60dea8
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-08-02 02:41:18 +0300

    [231] [Time Beats: 100.00%] [Memory Beats: 28.76%] - LeetPush

--- Power-of-Two.js
+++ Power-of-Two.js
@@ -3,13 +3,6 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    //O(1) solution using bit minpulation 
-    return n>0 && (n &(n-1)) ===0 ;
 
-    //using recursion 
-    /*
-    if (n === 1) return true;
-    if (n <= 0 || n % 2 !== 0) return false;
-    return isPowerOfTwo(n / 2);
-    */
+    return n>0 && (n &(n-1)) ===0 ;
 };
