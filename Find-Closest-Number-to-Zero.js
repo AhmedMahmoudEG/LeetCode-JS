630ac983d20942e28748d7e7fbcac332c4fb65b8
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-08-11 01:32:03 +0300

    [2239] [Time Beats: 49.87%] [Memory Beats: 23.48%] - LeetPush

---
+++ Find-Closest-Number-to-Zero.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findClosestNumber = function(nums) {
+    //[-100000,-100000]
+    let closest = nums[0];
+    
+    for (let x of nums) {
+        if (Math.abs(x) < Math.abs(closest)) {
+            closest = x;
+        }
+    }
+   	if (closest < 0 && nums.includes(Math.abs(closest))) {
+        return Math.abs(closest);
+    } else {
+        return closest;
+    }
+    
+};
