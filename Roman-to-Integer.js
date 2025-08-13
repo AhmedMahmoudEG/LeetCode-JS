9a0da3bd0752327ee397086f6e0dc3cbc711fdf0
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-08-14 00:46:41 +0300

    [13] [Time Beats: 53.59%] [Memory Beats: 44.06%] - LeetPush

--- Roman-to-Integer.js
+++ Roman-to-Integer.js
@@ -3,27 +3,16 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    //space O(1)
-  let obj = {
-    'I':1,
-    'V':5,
-    'X':10,
-    'L':50,
-    'C':100,
-    'D':500,
-    'M':1000
-  }
-  let res = 0;
-  //time O(n)
-  for(let i =0;i<s.length;i++){
-    let current = obj[s[i]];
-    let next = obj[s[i+1]]
-    if(current < next){
-        res += next - current;
-        i++
-    }else{
-        res +=current;
+    let roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    let res = 0
+    for(let i=0;i<s.length;i++){
+        let current = roman[s[i]];
+        let next = roman[s[i+1]] || 0 
+        if(current < next) {
+            res -=current
+        }else{
+            res +=current
+        }
     }
-  }
-  return res;
+    return res
 };
