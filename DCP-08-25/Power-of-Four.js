9949c340f79a467b9b47d025d993279ec53f430c
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-08-15 23:34:26 +0300

    [342] [Time Beats: 100.00%] [Memory Beats: 93.52%] - LeetPush

---
+++ DCP-08-25/Power-of-Four.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n<0) return false
+    while(n>1){
+        n = n/4
+    }
+    if(n==1) {return true 
+
+    } else return false
+};
