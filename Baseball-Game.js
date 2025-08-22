c1346d3ed116f9cf7b771bbf6843d6770703da59
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-08-22 03:03:27 +0300

    [682] [Time Beats: 81.16%] [Memory Beats: 41.97%] - LeetPush

--- Baseball-Game.js
+++ Baseball-Game.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var calPoints = function(operations) {
-    let res=0;
-    let stack =[];
-    
-    for(let i =0;i<operations.length;i++){
-        if(operations[i]==="C"){
-            stack.pop();
-        }else if(operations[i]=="D"){
-            stack.push(stack[stack.length-1]*2)
-        }else if (operations[i]=="+"){
-            stack.push(stack[stack.length-1]+stack[stack.length-2])
-        }else {
-            stack.push(parseInt(operations[i]))
+    let res =[];
+    for(let i=0;i<operations.length;i++){
+        if(operations[i]=='C'){
+            res.pop()
+        }else if(operations[i]=='D'){
+            res.push(res[res.length-1]*2)
+        }else if(operations[i]=='+'){
+            res.push(res[res.length-1]+res[res.length-2])
+        }else{
+            res.push(parseInt(operations[i]))
         }
     }
-    return stack.reduce((acc, val) => acc + val, 0);
+    return res.reduce((a,b)=>a+b,0)
 };
