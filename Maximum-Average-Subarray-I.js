bd93bcd8d18544b838e8d7650aa2e3876497ccdd
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-08-27 00:34:04 +0300

    [643] [Time Beats: 99.02%] [Memory Beats: 54.08%] - LeetPush

---
+++ Maximum-Average-Subarray-I.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    if(nums.length==1) return nums[0]/k
+    let sum=0;
+    for(let i =0;i<k;i++){
+        sum +=nums[i]
+    }
+    let maxSum=sum;
+    for(let i=k;i<nums.length;i++){
+        sum += nums[i] - nums[i-k]
+        maxSum =Math.max(sum,maxSum)
+    }
+    return maxSum / k
+};
