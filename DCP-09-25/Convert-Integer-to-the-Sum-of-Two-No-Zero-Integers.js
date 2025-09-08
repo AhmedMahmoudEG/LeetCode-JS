70739ea9da4107b0fa13cf00e45ef09d20c3994d
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-09-08 15:18:50 +0300

    [1317] [Time Beats: 100.00%] [Memory Beats: 23.19%] - LeetPush

---
+++ DCP-09-25/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var getNoZeroIntegers = function(n) {
+    function check(x){
+        return !x.toString().includes('0')
+    }
+    for(let i =1;i<n;i++){
+        let j = n-i;
+        if(check(i)&&check(j)){
+            return [i,j]
+        }
+    }
+};
