2c13498b42909300abbc5451ec6bf654a3800604
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-09-15 22:38:17 +0300

    [367] [Time Beats: 100.00%] [Memory Beats: 59.08%] - LeetPush

---
+++ Valid-Perfect-Square.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let low = 0;
+    let high = num
+
+    while(low<=high){
+        let mid = Math.floor((low+high)/2)
+        if(mid * mid == num){
+            return true 
+        }else if (mid * mid > num){
+            high = mid -1
+        }else{
+            low = mid + 1
+        }
+    }
+    return false
+};
