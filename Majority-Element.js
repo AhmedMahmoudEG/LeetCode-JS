2079c7ba83f9a61223fae24a05e566b565473662
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-09-26 15:34:24 +0300

    [169] [Time Beats: 45.76%] [Memory Beats: 59.14%] - LeetPush

--- Majority-Element.js
+++ Majority-Element.js
@@ -3,42 +3,17 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    //first solution using map
-    /*
     let map = new Map();
-    for(let n of nums){
-        map.set(n,(map.get(n)||0)+1)
+    for(let i=0;i<nums.length;i++){
+        map.set(nums[i],(map.get(nums[i])||0) +1)
     }
-    let maxValue = 0
-    for (let [key, value] of map) {
-    if (value > maxValue) {
-        maxValue = value;
-        maxKey = key;
-    }
-}
-    return maxKey
-    */
-    //solution 2 
-    /*
-    //sorting 
-    //O(nLogn)
-    nums = nums.sort((a,b)=>a-b)
-    //If you sort the array, the majority element must occupy the middle position.
-    //Why? Because it appears more than half the time, it must span the center.
-    return nums[Math.floor(nums.length / 2)];
-    */
-    //solution 3
-    let ans = nums[0]
-    let count = 1;
-    for(let i =1;i<nums.length;i++){
-        if(count ==0){
-            ans = nums[i]
-            count = 1
-        }else if(ans == nums[i]){
-            count++;
-        }else {
-            count --
+    let maxKey =null
+    let maxValue = 0;
+    for(let [key,value] of map.entries()){
+        if(value>maxValue){
+            maxValue =value
+            maxKey = key
         }
     }
-    return ans
+    return maxKey
 };
