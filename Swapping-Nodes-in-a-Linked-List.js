4fe971fb7c0f402e19818457dbebbc8d1f7f2ef5
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-11-09 22:41:01 +0200

    [1721] [Time Beats: 95.39%] [Memory Beats: 68.42%] - LeetPush

--- Swapping-Nodes-in-a-Linked-List.js
+++ Swapping-Nodes-in-a-Linked-List.js
@@ -11,31 +11,22 @@
  * @return {ListNode}
  */
 var swapNodes = function(head, k) {
-    let first = head;
-    let second = head;
-    let front = head;
-
-    // Move front to the k-th node
-    for (let i = 1; i < k; i++) {
+    let second =head;
+    let front =head
+    for(let i=1;i<k;i++){
         front = front.next;
     }
-
-    // Save reference to k-th node from start
-    let kthFromStart = front;
-
-    // Move front to the end, and move second simultaneously
-    while (front.next) {
+    //save referenec 
+    let kthStart = front;
+    while(front.next){
         front = front.next;
-        second = second.next;
+        second = second.next
     }
-
-    // Now, 'second' is k-th node from end
-    let kthFromEnd = second;
-
-    // Swap values
-    let temp = kthFromStart.val;
-    kthFromStart.val = kthFromEnd.val;
-    kthFromEnd.val = temp;
+    let kthEnd = second;
+    //swap values
+    let tmp = kthStart.val;
+    kthStart.val = kthEnd.val
+    kthEnd.val = tmp
 
     return head;
 };
