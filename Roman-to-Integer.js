62001363132981df5fe0b72b36e4b4477e3035f0
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-11-13 22:10:03 +0200

    [13] [Time Beats: 89.94%] [Memory Beats: 62.25%] - LeetPush

--- Roman-to-Integer.js
+++ Roman-to-Integer.js
@@ -3,16 +3,43 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-    let res = 0
+    let map = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000};
+    let res =0;
     for(let i=0;i<s.length;i++){
-        let current = roman[s[i]];
-        let next = roman[s[i+1]] || 0 
-        if(current < next) {
-            res -=current
+        let current = map[s[i]]
+        let next = map[s[i+1]]
+        if(next&& current <next){
+            res += next - current
+            i++
         }else{
             res +=current
         }
     }
-    return res
+    /*
+    for(let i=0;i<s.length;i++){
+        if(s[i]=="I"&& s[i+1]=="V") {
+        res+=4 
+        i++;
+        }else if(s[i]=="I"&&s[i+1]=="X"){
+             res+=9   
+             i++;
+        }else if(s[i]=="X"&&s[i+1]=="L"){
+             res+=40
+             i++;
+        }else if(s[i]=="X"&&s[i+1]=="C"){
+             res+=90
+             i++;   
+        }else if(s[i]=="C"&&s[i+1]=="D"){
+             res+=400
+             i++;
+        }else if(s[i]=="C"&&s[i+1]=="M"){
+             res+=900
+             i++;   
+        }else{
+            res +=map[s[i]]
+        }
+    }
+    */
+    
+    return res;
 };
