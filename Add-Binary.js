4528e20dc1503f05188ec80417ef1469bcf506c7
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-11-14 19:37:42 +0200

    [67] [Time Beats: 100.00%] [Memory Beats: 66.94%] - LeetPush

--- Add-Binary.js
+++ Add-Binary.js
@@ -4,11 +4,9 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    // If either binary string is longer than 52 bits,
-    // use BigInt to safely handle large binary numbers
-    if (a.length > 52 || b.length > 52)
-        return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
-    else
-        // For shorter binary strings, it's safe to use parseInt (base 2)
-        return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
+
+    if(a.length >52 ||b.length > 52){
+        return (BigInt('0b'+a)+BigInt('0b'+b)).toString(2);
+    }else 
+    return (parseInt(a,2)+parseInt(b,2)).toString(2);;
 };
