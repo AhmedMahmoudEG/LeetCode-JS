7a8c0784df2e0ac5dedecb7f757bc56206e63b2e
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-11-16 19:25:14 +0200

    [2] [Time Beats: 85.34%] [Memory Beats: 83.04%] - LeetPush

---
+++ Add-Two-Numbers.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let l3 =new ListNode(0);
+    let current = l3
+    let carry = 0;
+    while(l1!=null||l2!=null||carry!==0){
+        let x = l1 ? l1.val : 0; // لو اول ليست فيها هات الرقم الفاليو مفهاش حط صفر
+        let y = l2 ? l2.val : 0;
+        
+        let sum = x + y + carry; 
+        carry = Math.floor(sum / 10);  //باقي قسم المجموع لو كان عشرة ف الكاري هيبقى ب 1
+
+
+        current.next = new ListNode(sum % 10); //اي حاجة مود ال10 ب نفسها معدا العشرة هتبقى ب 0 في حالة وجود كاري 
+        
+        current = current.next;
+
+        if (l1 !== null) l1 = l1.next;
+        if (l2 !== null) l2 = l2.next;
+
+    }
+    return l3.next;
+};
