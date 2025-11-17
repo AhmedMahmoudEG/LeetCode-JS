2a1f80f6cee1c167f6085864a9f1b25c9a67e2bc
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-11-17 19:28:27 +0200

    [217] [Time Beats: 92.11%] [Memory Beats: 89.24%] - LeetPush

--- Contains-Duplicate.js
+++ Contains-Duplicate.js
@@ -3,14 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    /*
-    let set = new Set();
-    for (let i=0;i<nums.length;i++){
-        if(set.has(nums[i])) return true
-        set.add(nums[i])
-    }
-    return false
-    */
-    const set = new Set(nums);
-   return set.size !== nums.length
+    let set = new Set(nums);
+    return set.size !==nums.length
 };
