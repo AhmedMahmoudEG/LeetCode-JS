8300a39dfd88a64e9f6b4916c1572ffaa3534c22
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-11-19 18:40:04 +0200

    [349] [Time Beats: 100.00%] [Memory Beats: 35.45%] - LeetPush

--- Intersection-of-Two-Arrays.js
+++ Intersection-of-Two-Arrays.js
@@ -4,28 +4,12 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let map = new Map();
-    let res = new Set();
-    for(let i =0;i<nums1.length;i++){
-        map.set(nums1[i],1)
+    let p1=0;
+    let res =[]
+    let set = new Set()
+    for(let i of nums2){
+        if(nums1.includes(i)) set.add(i);
     }
-    for(let i=0;i<nums2.length;i++){
-        if(map.has(nums2[i])){
-            res.add(nums2[i])
-        }
-    }
-    return [...res]
-    //o(n^2)
-    /*
-    let res = new Set();
-    for(let i =0;i<nums1.length;i++){
-        for(let j = 0;j<nums2.length;j++){
-            if(nums1[i]==nums2[j]){
-            res.add(nums1[i]);
-            }
-            }
-        }
-    
-    return [...res]
-    */
+    return Array.from(set)
+ 
 };
