7a0c9f843c8d2c4dc4e9a8379d7803fe9ce6b10c
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-12-06 19:37:12 +0200

    [226] [Time Beats: 100.00%] [Memory Beats: 14.19%] - LeetPush

--- Invert-Binary-Tree.js
+++ Invert-Binary-Tree.js
@@ -1,25 +1,25 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-var invertTree = function(root) {
-    function dfs(node){
-        if(!node) return
-        let tmp = node.right
-        node.right = node.left
-        node.left =tmp
-        dfs(node.left)
-        dfs(node.right)
-    }
-     dfs(root)
-     return root
-    
-};
+1/**
+2 * Definition for a binary tree node.
+3 * function TreeNode(val, left, right) {
+4 *     this.val = (val===undefined ? 0 : val)
+5 *     this.left = (left===undefined ? null : left)
+6 *     this.right = (right===undefined ? null : right)
+7 * }
+8 */
+9/**
+10 * @param {TreeNode} root
+11 * @return {TreeNode}
+12 */
+13var invertTree = function(root) {
+14    function dfs(node){
+15        if(!node) return
+16        let tmp = node.right
+17        node.right = node.left
+18        node.left =tmp
+19        dfs(node.left)
+20        dfs(node.right)
+21    }
+22     dfs(root)
+23     return root
+24    
+25};
