09a2daeb80e80d3704aae4f20f7752a1ecbdfc92
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-12-08 19:38:07 +0200

    [100] [Time Beats: 100.00%] [Memory Beats: 63.03%] - LeetPush

--- Same-Tree.js
+++ Same-Tree.js
@@ -1,21 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} p
- * @param {TreeNode} q
- * @return {boolean}
- */
-var isSameTree = function(p, q) {
-    //base cases  when two trees are the same ? 
-    if(p===null&&q===null) return true;
-    if(p===null|| q===null) return false;
-    if(p.val!==q.val) return false;
-    //recall
-    return isSameTree(p.right,q.right)&& isSameTree(p.left,q.left)
-};
+1/**
+2 * Definition for a binary tree node.
+3 * function TreeNode(val, left, right) {
+4 *     this.val = (val===undefined ? 0 : val)
+5 *     this.left = (left===undefined ? null : left)
+6 *     this.right = (right===undefined ? null : right)
+7 * }
+8 */
+9/**
+10 * @param {TreeNode} p
+11 * @param {TreeNode} q
+12 * @return {boolean}
+13 */
+14var isSameTree = function(p, q) {
+15    if(q===null&&p===null) return true
+16    if(q===null ||p===null) return false
+17    if(q.val!==p.val) return false
+18   return isSameTree(q.left,p.left)&&isSameTree(q.right,p.right)
+19};
