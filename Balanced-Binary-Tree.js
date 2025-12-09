d22dfddd613307e6b8dde7ea1bc2dd00ed5bf213
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2025-12-09 20:55:00 +0200

    [110] [Time Beats: 11.32%] [Memory Beats: 75.79%] - LeetPush

--- Balanced-Binary-Tree.js
+++ Balanced-Binary-Tree.js
@@ -1,24 +1,23 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
-var isBalanced = function(root) {
-    function height(node){
-        if(!node) return 0
-        const left=height(node.left);
-        const right= height(node.right)
-        if(left == -1||right ==-1) return -1
-        if(Math.abs(left-right)> 1) return -1
-
-        return 1 + Math.max(left,right)
-    }
-    return height(root) !==-1
-};
+1/**
+2 * Definition for a binary tree node.
+3 * function TreeNode(val, left, right) {
+4 *     this.val = (val===undefined ? 0 : val)
+5 *     this.left = (left===undefined ? null : left)
+6 *     this.right = (right===undefined ? null : right)
+7 * }
+8 */
+9/**
+10 * @param {TreeNode} root
+11 * @return {boolean}
+12 */
+13var isBalanced = function(root) {
+14    function dfs(node){
+15        if(!node) return 0;
+16        return 1 + Math.max(dfs(node.left),dfs(node.right))
+17    }
+18    if(!root) return true
+19    let leftSide = dfs(root.left);
+20    let rightSide = dfs(root.right)
+21    if(Math.abs(leftSide-rightSide)>1) return false
+22    return isBalanced(root.left)&&isBalanced(root.right)
+23};
