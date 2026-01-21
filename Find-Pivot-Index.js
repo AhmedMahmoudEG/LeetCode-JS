e58d0825cec10e1698d5d153d1d5466f7e54d21b
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-01-21 15:18:46 +0200

    [724] [Time Beats: 50.17%] [Memory Beats: 13.54%] - LeetPush

--- Find-Pivot-Index.js
+++ Find-Pivot-Index.js
@@ -3,17 +3,16 @@
 3 * @return {number}
 4 */
 5var pivotIndex = function(nums) {
-6    let res = new Array(nums.length)
-7    res[0]=nums[0]
-8    for(let i =1;i<nums.length;i++){
-9        res[i]= res[i-1]+ nums[i]
+6    let prefix =[]
+7     prefix[0]=nums[0]
+8    for(let i=1;i<nums.length;i++){
+9        prefix[i] = prefix[i-1]+nums[i]
 10    }
-11    let totalSum =res[nums.length-1]
-12    for(let i=0;i<res.length;i++){
-13        let leftSum  = (i == 0) ? 0 : res[i - 1]
-14        let  rightSum = totalSum - res[i]
-15        if(leftSum==rightSum) return i
-16
-17    }
-18    return -1
-19};
+11    let total=prefix[nums.length-1]
+12    for(let i =0;i<nums.length;i++){
+13        let left = (i==0) ? 0 : prefix[i-1]
+14        let right = total - prefix[i]
+15        if(left == right) return i
+16    }
+17    return -1
+18};
