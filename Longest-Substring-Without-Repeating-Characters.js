4f26723d66b912428ed936eb5fed22883f6fd78d
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-01-29 23:01:16 +0200

    [3] [Time Beats: 47.05%] [Memory Beats: 58.04%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.js
+++ Longest-Substring-Without-Repeating-Characters.js
@@ -1,23 +1,22 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLongestSubstring = function(s) {
-    let longest = new Map();
-    //space O(m)
-    //time O(n)
-        let maxLength = 0
-        let start = 0
-        for(let i =0;i<s.length;i++){
-            //لو الحرف اتكرر حرك البداية خطوة لقدام
-            if(longest.has(s[i])){
-                start = Math.max(start, longest.get(s[i]) + 1)
-            }
-            //لو مشفتهوش سجله وحطه الفاليو بتاعته الاندكس 
-                longest.set(s[i],i)
-                //قارن المسافة الحالية ب المسافة الجديد من حيث الاكبر 
-                maxLength = Math.max(maxLength, i - start + 1)
-            
-        }
-        return maxLength;
-};
+1/**
+2 * @param {string} s
+3 * @return {number}
+4 */
+5var lengthOfLongestSubstring = function(s) {
+6    let set = new Set();
+7    let l =0;
+8    let max = 0
+9    for(let r=0;r<s.length;r++){
+10        if(!set.has(s[r])){
+11            set.add(s[r])
+12        }else{
+13            while(set.has(s[r])){
+14            set.delete(s[l])
+15            l++;
+16            }
+17            set.add(s[r])
+18        }
+19            max= Math.max(max,r-l+1)
+20    }
+21    return max
+22};
