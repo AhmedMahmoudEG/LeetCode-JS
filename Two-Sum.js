eecf43f2f0319f4878ea715cc840eba666978d83
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-01-30 23:41:34 +0200

    [1] [Time Beats: 87.48%] [Memory Beats: 66.79%] - LeetPush

--- Two-Sum.js
+++ Two-Sum.js
@@ -1,15 +1,20 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    let map = new Map();
-    //[2,7,11,15] t=9 ex=[0,1]
-    for (let i=0;i<nums.length;i++){
-        if(map.has(target-nums[i])){
-            return [i,map.get(target-nums[i])]
-        }
-        map.set(nums[i],i)
-    }
-};
+1/**
+2 * @param {number[]} nums
+3 * @param {number} target
+4 * @return {number[]}
+5 */
+6var twoSum = function(nums, target) {
+7    let map = new Map();
+8    for(let i =0;i<nums.length;i++){
+9        let need = target - nums[i];
+10        if(map.has(need))  return [map.get(need), i];
+11        map.set(nums[i],i)
+12    }
+13    /*
+14    for(let i =0;i<nums.length;i++){
+15        for(let j=i+1;j<nums.length;j++){
+16            if(nums[i]+nums[j]==target) return [i,j]
+17        }
+18    }
+19    */
+20};
