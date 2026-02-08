386d2f4b103b60ce46f83caad6c3b89bb145f84f
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-02-09 00:41:15 +0200

    [110] [Time Beats: 13.30%] [Memory Beats: 77.18%] - LeetPush

--- Balanced-Binary-Tree.js
+++ Balanced-Binary-Tree.js
@@ -11,13 +11,13 @@
 11 * @return {boolean}
 12 */
 13var isBalanced = function(root) {
-14    function dfs(node){
-15        if(!node) return 0;
-16        return 1 + Math.max(dfs(node.left),dfs(node.right))
+14    function height(node){
+15        if(!node) return 0
+16        return 1 + Math.max(height(node.left), height(node.right))
 17    }
 18    if(!root) return true
-19    let leftSide = dfs(root.left);
-20    let rightSide = dfs(root.right)
-21    if(Math.abs(leftSide-rightSide)>1) return false
-22    return isBalanced(root.left)&&isBalanced(root.right)
+19    let left = height(root.left)
+20    let right = height( root.right)
+21    if(Math.abs(left - right) >1 ) return false
+22    return isBalanced(root.left)&& isBalanced(root.right)
 23};
