3ca7221fbee62727f1e0084258be958132b994e2
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-02-15 21:46:41 +0200

    [67] [Time Beats: 100.00%] [Memory Beats: 89.49%] - LeetPush

--- Add-Binary.js
+++ Add-Binary.js
@@ -1,12 +1,12 @@
-/**
- * @param {string} a
- * @param {string} b
- * @return {string}
- */
-var addBinary = function(a, b) {
-
-    if(a.length >52 ||b.length > 52){
-        return (BigInt('0b'+a)+BigInt('0b'+b)).toString(2);
-    }else 
-    return (parseInt(a,2)+parseInt(b,2)).toString(2);;
-};
+1/**
+2 * @param {string} a
+3 * @param {string} b
+4 * @return {string}
+5 */
+6var addBinary = function(a, b) {
+7    if(a.length>52 || b.length>52){
+8        return (BigInt('0b'+a) + BigInt('0b'+b)).toString(2)
+9    }else{
+10        return (parseInt(a,2)+parseInt(b,2)).toString(2)
+11    }
+12};
