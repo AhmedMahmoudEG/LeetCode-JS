63aaa75975af0d73656ebe3ace173b8ecac27ecf
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-02-27 00:29:00 +0200

    [35] [Time Beats: 100.00%] [Memory Beats: 31.01%] - LeetPush

--- Search-Insert-Position.js
+++ Search-Insert-Position.js
@@ -1,20 +1,21 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-var searchInsert = function(nums, target) {
-    let low=0;
-    let high = nums.length-1;
-    while(low<=high){
-        let mid = Math.floor((low+high)/2);
-        if(nums[mid]==target){
-            return mid
-        }else if(nums[mid]>target){
-            high = mid - 1
-        }else{
-            low = mid +1
-        }
-    }
-    return low
-};
+1/**
+2 * @param {number[]} nums
+3 * @param {number} target
+4 * @return {number}
+5 */
+6var searchInsert = function(nums, target) {
+7    let low = 0;
+8    let high = nums.length-1
+9    while(low<=high){
+10        let mid  = Math.floor((low+high)/2)
+11        if(nums[mid]>target){
+12            high = mid -1
+13        }else if(nums[mid]<target){
+14
+15            low  = mid +1
+16        }else{
+17            return mid
+18        }
+19    }
+20    return low
+21};
