d5309be613c73308067cb63af5fdf44cdab10925
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-03-21 23:16:21 +0200

    [13] [Time Beats: 39.65%] [Memory Beats: 89.79%] - LeetPush

--- Roman-to-Integer.js
+++ Roman-to-Integer.js
@@ -1,45 +1,20 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    let map = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000};
-    let res =0;
-    for(let i=0;i<s.length;i++){
-        let current = map[s[i]]
-        let next = map[s[i+1]]
-        if(next&& current <next){
-            res += next - current
-            i++
-        }else{
-            res +=current
-        }
-    }
-    /*
-    for(let i=0;i<s.length;i++){
-        if(s[i]=="I"&& s[i+1]=="V") {
-        res+=4 
-        i++;
-        }else if(s[i]=="I"&&s[i+1]=="X"){
-             res+=9   
-             i++;
-        }else if(s[i]=="X"&&s[i+1]=="L"){
-             res+=40
-             i++;
-        }else if(s[i]=="X"&&s[i+1]=="C"){
-             res+=90
-             i++;   
-        }else if(s[i]=="C"&&s[i+1]=="D"){
-             res+=400
-             i++;
-        }else if(s[i]=="C"&&s[i+1]=="M"){
-             res+=900
-             i++;   
-        }else{
-            res +=map[s[i]]
-        }
-    }
-    */
-    
-    return res;
-};
+1/**
+2 * @param {string} s
+3 * @return {number}
+4 */
+5var romanToInt = function(s) {
+6    let values = {'I':1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000};
+7    let res = 0
+8    for(let i =0;i<s.length;i++){
+9        let curr = values[s[i]]
+10        let next = values[s[i+1]]
+11        if(next&&curr <next ){
+12            res += next - curr
+13            i++
+14        }else{
+15            res += curr
+16        }
+17
+18    }
+19    return res
+20};
