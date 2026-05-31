4fb97f17ba1fbccf7951b5c56e1e5e24462667f2
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-06-01 00:16:09 +0300

    [14] [Time Beats: 100.00%] [Memory Beats: 95.19%] - LeetPush

--- Longest-Common-Prefix.js
+++ Longest-Common-Prefix.js
@@ -1,18 +1,19 @@
-/**
- * @param {string[]} strs
- * @return {string}
- */
-var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return "";
-
-    let prefix = strs[0];
-
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.slice(0, -1);
-            if (prefix === "") return "";
-        }
-    }
-
-    return prefix;
-};
+1/**
+2 * @param {string[]} strs
+3 * @return {string}
+4 */
+5var longestCommonPrefix = function(strs) {
+6    if (strs.length === 0) return "";
+7
+8    let prefix = strs[0];
+9
+10    for (let i = 1; i < strs.length; i++) {
+11        while (strs[i].indexOf(prefix) !== 0) {
+12            prefix = prefix.slice(0, -1);
+13            if (prefix === "") return "";
+14        }
+15    }
+16
+17    return prefix;
+18    }
+19
