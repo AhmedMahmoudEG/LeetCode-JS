721b638e78982d5fd86ce3aff5abd6f5f2b7fc35
Ahmed Mahmoud <60559798+AhmedMahmoudEG@users.noreply.github.com>
2026-06-08 21:00:40 +0300

    [58] [Time Beats: 100.00%] [Memory Beats: 59.86%] - LeetPush

--- Length-of-Last-Word.js
+++ Length-of-Last-Word.js
@@ -1,19 +1,10 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLastWord = function(s) {
-    /*
-    let arr = s.trim().split(" ").filter(word=>word.length>0);
-    if(s.length==0)return 0
-    return arr[arr.length-1].length;
-    */
-    s = s.trim();
-    let last = s.length-1;
-    let str ="";
-    while(last>=0&&s.charAt(last)!=" "){
-        str += s.charAt(last);
-        last--;
-    }
-    return str.length;
-};
+1/**
+2 * @param {string} s
+3 * @return {number}
+4 */
+5var lengthOfLastWord = function(s) {
+6    let arr = s.trim().split(" ").filter(word=>word.length>0);
+7        if(s.length==0)return 0
+8        console.log(arr[arr.length-1])
+9        return arr[arr.length-1].length;
+10};
